c112bf02c1477b006d5f7e362a2566d8f9243e0c
Jaka <jakanggara@gmail.com>
2020-10-24 10:54:42 +0700

    new datas

---
+++ src/datas/projects.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: "Geologocal Mapping Database",
+    desc: "Plaform to publish geological map in svg",
+    status: 0,
+  },
+  {
+    title: "Geological Computation",
+    desc: "",
+    status: 0,
+  },
+  {
+    title: "TODO List",
+    desc: "Simple CRUD on mobile apps with flutter.",
+    status: 0,
+  },
+]
