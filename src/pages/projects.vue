2e5d93470bd26441638126d9916473cda8df6dc2
Jaka <jakanggara@gmail.com>
2020-10-27 10:32:34 +0700

    chg: card layout

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,22 +1,24 @@
 <template>
   <b-container id="projects">
     <h1 class="mb-4">My Projects</h1>
-    <b-media>
-      <b-card
+    <b-row>
+      <b-col sm="6" class="px-2"
         v-for="item in projects"
         :key="item.desc"
-        :to="item.title"
-        :border-variant="statusHandle(item.status, 1)"
-        class="mb-3 shadow">
-        <b-card-title class="mb-1">
-          {{ item.title }}
-        </b-card-title>
-        <b-card-sub-title class="mb-2">
-          <b-badge pill :variant="statusHandle(item.status, 1)"> {{statusHandle(item.status)}}</b-badge>
-        </b-card-sub-title>
-        <p class="mb-1">{{ item.desc }}</p>
-      </b-card>
-    </b-media>
+        :to="item.title">
+        <b-card
+          :border-variant="statusHandle(item.status, 1)"
+          class="mb-3 shadow">
+          <b-card-title class="mb-1">
+            {{ item.title }}
+          </b-card-title>
+          <b-card-sub-title class="mb-2">
+            <b-badge pill :variant="statusHandle(item.status, 1)"> {{statusHandle(item.status)}}</b-badge>
+          </b-card-sub-title>
+          <p class="mb-1">{{ item.desc }}</p>
+        </b-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
