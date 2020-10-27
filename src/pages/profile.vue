44a6507eecf55408cfad75ee2db18447bf8d6d72
Jaka <jakanggara@gmail.com>
2020-10-27 20:22:39 +0700

    card view

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,27 +1,37 @@
 <template>
   <b-container class="about">
-    <div>
+    <b-media>
       <h1>My Profile</h1>
-      This page shows my profile as an individual and it should describes my skills as clear as possible.
-    </div>
-    <div>
-      I am a Geological Graduate that have great interest in Recent Technologies. I also developed skills to make web-apps and mobile apps.
-    </div>
-    <div>
-      <h1>Skillsets</h1>
-      <ul>
-        <li>Python</li>
-        <li>HTML CSS</li>
-        <li>JS</li>
-        <li>Vue</li>
-        <li>Flutter</li>
-        <li>ArcGIS</li>
-        <li>QGIS</li>
-        <li>Office Suites (Presentation / Sheets / Words)</li>
-        <li>Geological Mapping</li>
-      </ul>
-      <h2>Conceptual Skills</h2>
-      <h2>Technical Skills</h2>
-    </div>
+      <p>
+        I am a Geological Graduate that have great interest in Recent Technologies. I also developed skills to make web-apps and mobile apps.
+      </p>
+    </b-media>
+    <b-media>
+      <h2>Skillsets</h2>
+      <b-card v-for="item in skills" :key="item.title">
+        <p>{{ item.title }}</p>
+        <b-img :src="imgResolve(item.img)" height="50px" right />
+      </b-card>
+    </b-media>
   </b-container>
 </template>
+
+<script>
+import dataSkills from "@/datas/skills";
+export default {
+  data() {
+    return {
+      skills: dataSkills
+    }
+  },
+  methods: {
+    imgResolve(img) {
+      return `static/${img}`;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
