8b354fe07279135bf466543052097d85d3237b57
Jaka <jakanggara@gmail.com>
2020-10-27 08:20:40 +0700

    diplayed w/ cards

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,11 +1,22 @@
 <template>
   <b-container id="projects">
-    <h1>My Projects</h1>
-    <b-nav>
-      <b-nav-item v-for="item in projects" :key="item.desc" :to="item.title">
-        {{ item.title }}
-      </b-nav-item>
-    </b-nav>
+    <h1 class="mb-4">My Projects</h1>
+    <b-media>
+      <b-card
+        v-for="item in projects"
+        :key="item.desc"
+        :to="item.title"
+        :border-variant="statusHandle(item.status, 1)"
+        class="mb-3 shadow">
+        <b-card-title class="mb-1">
+          {{ item.title }}
+        </b-card-title>
+        <b-card-sub-title class="mb-2">
+          <b-badge pill :variant="statusHandle(item.status, 1)"> {{statusHandle(item.status)}}</b-badge>
+        </b-card-sub-title>
+        <p class="mb-1">{{ item.desc }}</p>
+      </b-card>
+    </b-media>
   </b-container>
 </template>
 
@@ -15,6 +26,23 @@ export default {
   data() {
     return {
       projects: dataProjects,
+    }
+  },
+  methods: {
+    statusHandle(status, vv) {
+      if (status==0) {
+        if (vv) {return "danger"}
+        return "deprecated";
+      } else if (status==1) {
+        if (vv) {return "success"}
+        return "maintained";
+      } else if (status==2) {
+        if (vv) {return "primary"}
+        return "in progress";
+      } else {
+        if (vv) {return "secondary"}
+        return "unknown";
+      }
     }
   }
 }
