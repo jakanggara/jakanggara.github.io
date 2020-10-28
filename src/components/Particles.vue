92c64071219b287ba25d28f789972e4719e0d8fa
Jaka <jakanggara@gmail.com>
2020-10-29 05:09:08 +0700

    added: particlesjs

---
+++ src/components/Particles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :id="this.domId" class="">
+  </div>
+</template>
+
+<script>
+import "particles.js";
+export default {
+  name: "VueParticleJs",
+  props: {
+    config: Object,
+    domId: String
+  },
+  mounted() {
+    if (typeof this.domId !== "string") throw new Error("Invalid Dom object Provided");
+    window.particlesJS(this.domId, {
+      particles:this.config.particles,
+      interactivity:this.config.interactivity,
+      retina_detect:this.config.retina_detect
+    }, function() {
+      // eslint-disable-next-line no-console
+      console.log("callback - particles.js config loaded");
+    });
+  }
+};
+</script>
--- src/layouts/layout2.vue
+++ src/layouts/layout2.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="layout">
-    <router-view />
-  </div>
+  <b-container fluid class="overflow-hidden">
+    <Particles domId="coba" :config=ParticlesData class="layout"></Particles>
+    <router-view class="position-absolute fixed-top" />
+  </b-container>
 </template>
+
+<script>
+import Particles from "@/components/Particles.vue";
+import ParticlesData from "@/datas/particlesjs-config.json";
+
+export default {
+  components: {
+    Particles
+  },
+  data() {
+    return {
+      ParticlesData
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
