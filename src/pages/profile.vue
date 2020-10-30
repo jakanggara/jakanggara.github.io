43e8a3e51b838ee97dede0be2ca130d09ded173a
Jaka <jakanggara@gmail.com>
2020-10-31 00:03:16 +0700

    add modal

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,5 +1,13 @@
 <template>
   <b-container id="profile" class="zero-scrollbar">
+    <b-modal :title="datashow.title" v-model="detail" centered hide-footer>
+      <b-media right-align>
+        <template>
+          <b-img :src="imgResolve(datashow.img)" height="100px" right />
+        </template>
+        <p>{{ datashow.desc }}</p>
+      </b-media>
+    </b-modal>
     <b-media>
       <h1>My Profile</h1>
       <p>
@@ -17,7 +25,7 @@
           v-for="item in skills"
           :key="item.title"
         >
-          <b-card>
+          <b-card v-on:click="toggleSkill(item)">
             <b-card-title>
               {{ item.title }}
             </b-card-title>
@@ -25,7 +33,6 @@
               <template>
                 <b-img :src="imgResolve(item.img)" height="50px" right />
               </template>
-              <p>{{ item.desc }}</p>
             </b-media>
           </b-card>
         </b-col>
@@ -39,13 +46,38 @@ import dataSkills from "@/datas/skills";
 export default {
   data() {
     return {
+      datashow: {
+        title: "",
+        img: "",
+        desc: "",
+      },
+      detail: 'none',
       skills: dataSkills
     };
   },
   methods: {
     imgResolve(img) {
       return `static/${img}`;
+    },
+    toggleSkill(x) {
+      if(x==0) {
+        this.detail="none";
+      } else {
+        this.detail="block";
+        this.datashow = x;
+      }
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .z999 {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    background-color: beige;
+  }
+</style>
