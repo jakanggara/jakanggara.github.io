4f040e1b53ba9815f813f1a08730ac3f6ce3a943
jakanggara <jakanggara@gmail.com>
2020-10-31 02:14:14 +0700

    spa prerender

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   module: {
     rules: [
@@ -10,6 +13,17 @@ module.exports = {
         use: ["vue-style-loader", "css-loader", "sass-loader"]
       }
     ]
-  }
+  },
   // plugin omitted
+  plugins: [
+    ...
+    new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, 'dist'),
+  
+      // Required - Routes to render.
+      routes: [ '/' ],
+    })
+  ]
+
 };
