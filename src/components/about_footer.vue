f47633c1ebc380fd590d2d8bdafd6291572c1a24
Jaka <jakanggara@gmail.com>
2020-10-31 00:02:08 +0700

    changed footer

--- src/components/about_footer.vue
+++ src/components/about_footer.vue
@@ -3,25 +3,40 @@
     <b-row
       no-gutters
       align-v="center"
-      class="container-sm ml-auto mr-auto pt-3 footerdiv"
+      align-h="center"
+      class="container-sm ml-auto mr-auto pt-3 footerdiv w-25 d-none d-sm-flex text-center"
     >
-      <b-col cols="2" sm="2" align-self="center">
-        <b-img
-          center
-          src="@/assets/images/logo.svg"
-          width="40px"
-          alt="Jaka Anggara Logo"
-        />
-      </b-col>
-      <b-col cols="10" sm="10">
-        <faIcon :icon="['fas', 'envelope']" /> : jakanggara@outlook.com
-      </b-col>
-      <b-row no-gutters style="width: 100%">
-        <b-col class="text-center">
-          &copy;2020 Jakanggara
+      <a href="mailto:jakanggara@outlook.com">
+        <b-col>
+          <faIcon :icon="['fas', 'envelope']" />
+          <span class="d-block">Mail Me!</span>
         </b-col>
-      </b-row>
+        <b-row>
+          <b-col>
+          </b-col>
+        </b-row>
+        <b-row no-gutters style="width: 100%">
+          <b-col>
+            &copy;2020 Jakanggara
+          </b-col>
+        </b-row>
+      </a>
     </b-row>
+    <div
+      class="text-center ml-auto mr-auto pt-3 footerdiv w-75 d-sm-none"
+      >
+      <a href="mailto:jakanggara@outlook.com">
+        <div>
+          <faIcon :icon="['fas', 'envelope']" />
+          <span class="d-block">
+            Mail Me!
+          </span>
+        </div>
+        <div>
+          &copy;2020 Jakanggara
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -34,5 +49,12 @@ export default {};
   background-color: $color2;
   border-top-left-radius: 10px 10px;
   border-top-right-radius: 10px 10px;
+  a{
+    text-decoration: none;
+    color: inherit;
+    &:hover {
+      color: $ccolor1;
+    }
+  }
 }
 </style>
